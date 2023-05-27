2d46ae473763902a006bc81a18ab92f2b93a8c81
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-27 00:34:14 -0700

    nav jsx

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './index.scss'
+
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="nav-bar">
         <nav>
           <ul>
-            <li>Logo</li>
-            <li>Home</li>
-            <li>Portfolio</li>
-            <li>Services</li>
-            <li>Contact</li>
+            <li className="logo">Logo</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
           </ul>
         </nav>
       </div>
