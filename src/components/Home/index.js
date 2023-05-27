fea80f13d2e3769e26127bd9b615f824d664d86c
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-27 00:41:47 -0700

    components

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Loader from 'react-loaders'
+
+
+const Home = () => {
+    return(
+        <>
+        
+        <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Home;
