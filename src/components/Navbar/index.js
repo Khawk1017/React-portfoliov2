1f11fdcf81d03d785c3042435377218ff418baf7
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-28 13:39:52 -0700

    removed logo text

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,7 +8,6 @@ const Navbar = () => {
       <div className="nav-bar">
         <nav>
           <ul>
-            <li className="logo">Logo</li>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact</Link></li>
