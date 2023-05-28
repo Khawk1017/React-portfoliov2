7c44fbcb24d8bc4982cbb05e8ce41ed7dcb7e7cb
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-28 13:37:06 -0700

    imported about route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import About from './components/About';
 
 import { BrowserRouter as Router,  Routes, Route } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
       <Footer />
       <Routes>
       <Route exact path="/" Component={Home} />
+      <Route exact path="/about" Component={About} />
       </Routes>
     </div>
    </Router>
