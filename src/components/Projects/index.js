37a3682862d8445e9d617ba68d7f4eab0a676378
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-05-30 17:39:30 -0700

    fixed heigharchy for images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,18 +1,34 @@
 import './index.scss'
 import Loader from 'react-loaders'
 import Jate from '../../assets/img/Jate.png'
-import NoteTaker from   '../../assets/img/NoteTaker.png'
+import NoteTaker from   '../../assets/img/Note2.png'
 import Password2 from '../../assets/img/Password2.png'
+import SocialNet from '../../assets/img/SocialNetworkAPI.png'
+import PublicInterest from '../../assets/img/PublicIntrest.png'
 
 const Projects = () => {
     return (
         <>
-           <div className="projects">
-            <div className="projects__bio-image">
-                <h1 className="text-secondary">My Projects</h1>
+           <div className='projects'>
+           <div className='project-item'>
+           <h2>Password Generator</h2>
+            <img src={Password2} alt='password'></img>
             </div>
-            <div className='image-container'>
-                <img src={Jate} alt='jate' />
+            <div className='project-item'>
+           <h2>PWA Text Editor app</h2>
+            <img src={Jate} alt='Jate'></img>
+            </div>
+            <div className='project-item'>
+           <h2>NoteTaker (Express.js)</h2>
+            <img src={NoteTaker} alt='Note'></img>
+            </div>
+            <div className='project-item'>
+           <h2>SocialNetworkAPI (MongoDB, Express.js)</h2>
+            <img src={SocialNet} alt='SocialNet'></img>
+            </div>
+            <div className='project-item'>
+           <h2>Public Interest Press (MVC)</h2>
+            <img src={PublicInterest} alt='Public Interest'></img>
             </div>
            </div>
             <Loader type='pacman' />
