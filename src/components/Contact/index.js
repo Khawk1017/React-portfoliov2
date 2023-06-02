7ea8d23eca349d6bb0b9ef3c09908c4d0a5c6c30
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-06-01 17:23:18 -0700

    updtated form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import Loader from "react-loaders";
 import emailjs from '@emailjs/browser'
+import './index.scss'
 
 export const Contact = () => {
     const form = useRef()
@@ -17,44 +18,47 @@ export const Contact = () => {
 
     return (
         <>
-            <div className='contact-form'>
-            <form ref={form} onSubmit={sendEmail}>
-                            <ul>
-                                <li className='half'>
-                                    <input
-                                        type="text"
-                                        name="user_name"
-                                        placeholder="Name"
-                                        required
-                                    />
-                                </li>
-                                <li className='half'>
-                                    <input
-                                        type="email"
-                                        name="user_email"
-                                        placeholder="Email"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <input
-                                        placeholder="Subject"
-                                        type="text"
-                                        name="subject"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <textarea
-                                        placeholder="Message"
-                                        name="message"
-                                        required></textarea>
-                                </li>
-                                <li>
-                                    <input type="submit" className="flat-button" value="SEND" />
-                                </li>
-                            </ul>
-                        </form>
+            <div className='contact-container'>
+                <div className='contact-form'>
+                    <h1>Contact Me</h1>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <ul>
+                            <li className='half'>
+                                <input
+                                    type="text"
+                                    name="user_name"
+                                    placeholder="Name"
+                                    required
+                                />
+                            </li>
+                            <li className='half'>
+                                <input
+                                    type="email"
+                                    name="user_email"
+                                    placeholder="Email"
+                                    required
+                                />
+                            </li>
+                            <li>
+                                <input
+                                    placeholder="Subject"
+                                    type="text"
+                                    name="subject"
+                                    required
+                                />
+                            </li>
+                            <li>
+                                <textarea
+                                    placeholder="Message"
+                                    name="message"
+                                    required></textarea>
+                            </li>
+                            <li>
+                                <input type="submit" className="flat-button" value="SEND" />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
             <Loader type="pacman" />
         </>
